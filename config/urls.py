c615f8592c57cd09e9a73ae1b9ee7563da610515
Saidova <saidova.kseniya@mail.ru>
2025-11-24 21:17:47 +0300

    В главном config/urls.py добавлены эндпоинты для аутентификации

--- config/urls.py
+++ config/urls.py
@@ -20,19 +20,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # Админка Django
     path('admin/', admin.site.urls),
-
-    # Документация API
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-    # API URLs
-    path('api/', include([
-        # Привычки
-        path('habits/', include('habits.urls')),
-
-        # Пользователи (добавим позже)
-        # path('users/', include('users.urls')),
-    ])),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('habits.urls')),
+    path('api/users/', include('users.urls')), # Для регистрации
 ]
