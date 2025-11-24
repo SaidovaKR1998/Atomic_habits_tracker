cf8dce10de2d4bc3ba102c23eff4a6ba5879ab9c
Saidova <saidova.kseniya@mail.ru>
2025-11-24 21:44:45 +0300

    Добавляем аутентификацию JWT. Обновляем habits/views.py

--- habits/views.py
+++ habits/views.py
@@ -1,18 +1,25 @@
 # habits/views.py
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 from .models import Habit
 from .serializers import HabitSerializer
 
-
-# ВРЕМЕННО отключаем аутентификацию для тестирования
 class HabitViewSet(viewsets.ModelViewSet):
+    """ViewSet для CRUD операций с привычками"""
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Habit.objects.all()
+        # Пользователь видит только свои привычки
+        return Habit.objects.filter(user=self.request.user)
 
+    def perform_create(self, serializer):
+        # При создании привычки автоматически назначаем текущего пользователя
+        serializer.save(user=self.request.user)
 
 class PublicHabitListAPIView(ListAPIView):
+    """Эндпоинт для списка публичных привычек"""
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
     queryset = Habit.objects.filter(is_public=True)
