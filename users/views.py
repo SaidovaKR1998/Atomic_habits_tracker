0e0a91fbfa2189a43cf78cb20af9e41e1edb7d2e
Saidova <saidova.kseniya@mail.ru>
2025-11-24 21:45:39 +0300

    Создаем приложение users для регистрации users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# users/views.py
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserCreateAPIView(generics.CreateAPIView):
+    """Регистрация нового пользователя"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
