47cd0d2f0455a3f60ea9633f39f7a89f61c38d88
Saidova <saidova.kseniya@mail.ru>
2025-11-24 21:02:02 +0300

    Создаем вьюшки (habits/views.py):

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Habit
+from .serializers import HabitSerializer
+from .pagination import HabitsPagination
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitsPagination
+
+    def get_queryset(self):
+        # Пользователь видит только свои привычки
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # При создании привычки автоматически назначаем текущего пользователя
+        serializer.save(user=self.request.user)
+
+class PublicHabitListAPIView(ListAPIView):
+    """ Эндпоинт для списка публичных привычек """
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitsPagination
+    queryset = Habit.objects.filter(is_public=True)
+
