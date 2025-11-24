0608c549ff6140f4a451582fc34c917a7bc175e0
Saidova <saidova.kseniya@mail.ru>
2025-11-24 21:06:13 +0300

    Корректировка (habits/views.py)

--- habits/views.py
+++ habits/views.py
@@ -1,14 +1,14 @@
+# habits/views.py
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
 from .models import Habit
 from .serializers import HabitSerializer
-from .pagination import HabitsPagination
-from rest_framework.generics import ListAPIView
 
 class HabitViewSet(viewsets.ModelViewSet):
+    """ViewSet для CRUD операций с привычками"""
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = HabitsPagination
 
     def get_queryset(self):
         # Пользователь видит только свои привычки
@@ -19,9 +19,7 @@ class HabitViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 class PublicHabitListAPIView(ListAPIView):
-    """ Эндпоинт для списка публичных привычек """
+    """Эндпоинт для списка публичных привычек"""
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = HabitsPagination
     queryset = Habit.objects.filter(is_public=True)
-
