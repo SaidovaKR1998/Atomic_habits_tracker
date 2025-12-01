db7c4545d2a39f3e245bf8a5bbb0595b402732a8
Saidova <saidova.kseniya@mail.ru>
2025-12-01 19:26:46 +0300

    Добавим поле для Telegram chat ID. Создадим users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+# users/models.py
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telegram_chat_id = models.CharField(max_length=20, blank=True, null=True, verbose_name='Telegram Chat ID')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
+# Сигналы для автоматического создания профиля
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
