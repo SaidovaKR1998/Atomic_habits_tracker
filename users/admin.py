5dedb85c8cedb9e4468187ed32a3d3a81fa97efd
Saidova <saidova.kseniya@mail.ru>
2025-12-01 19:28:14 +0300

    Регистрируем модель в админке users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+# Перерегистрируем User
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
